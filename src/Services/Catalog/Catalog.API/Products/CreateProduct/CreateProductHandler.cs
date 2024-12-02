3474d18550c502b4cb91b62031eaa52844851e49
Muhammad Umar Baig <Muhammad Umar Baig@DESKTOP-JPQUJSN>
2024-12-03 01:18:12 +0500

    product section

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,List<string> Category,string Description,string ImageFile,decimal Price)
+    :ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler (IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create product entity from incoming command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+            Category = command.Category,
+        };
+        //save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        //return CreateProductResult object
+        return new CreateProductResult(product.Id);
+
+    }
+}
